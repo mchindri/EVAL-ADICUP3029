53b2998973d6ca475c68447d47b38ddbcd09984c
Mihail Chindris <mihail.chindris@analog.com>
2020-10-30 10:06:44 +0200

    project:demo_esp8266: Update Makefile to new format
    Update as new no-os Makefile format
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- projects/ADuCM3029_demo_esp8266/src.mk
+++ projects/ADuCM3029_demo_esp8266/src.mk
@@ -1,33 +1,8 @@
-################################################################################
-#									       #
-#     Shared variables:							       #
-#	- PROJECT							       #
-#	- DRIVERS							       #
-#	- INCLUDE							       #
-#	- PLATFORM_DRIVERS						       #
-#	- NO-OS								       #
-#									       #
-################################################################################
+SRC_DIRS += $(PROJECT)/app_src
+SRC_DIRS += $(PLATFORM_DRIVERS)
+SRC_DIRS += $(INCLUDE)
+SRC_DIRS += $(NO-OS)/util
+SRC_DIRS += $(NO-OS)/network
+SRC_DIRS += $(DRIVERS)/accel/adxl362
 
-DISABLE_SECURE_SOCKET=y
-
-SRC_DIRS +=	$(PROJECT)/app_src		\
-		$(PLATFORM_DRIVERS)		\
-		$(INCLUDE)			\
-		$(NO-OS)/util			\
-		$(NO-OS)/network		\
-		$(DRIVERS)/accel/adxl362 \
-		$(NO-OS)/libraries/mqtt			
-
-#Include makefiles from each source directory if they exist
-SUB_MAKES= $(wildcard $(addsuffix /src.mk, $(SRC_DIRS)))
-include $(SUB_MAKES)
-
-DIRECORIES_WITH_MAKEFILES = $(patsubst %/src.mk, %,$(SUB_MAKES))
-REMAINING_DIRECORIES = $(filter-out $(DIRECORIES_WITH_MAKEFILES), $(SRC_DIRS))
-
-# recursive wildcard
-rwildcard = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
-
-SRCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.c))
-INCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.h))
+LIBRARIES += mqtt
